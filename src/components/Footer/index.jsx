b930ef0c227ca089605c795ec7274936ad64b0f7
Alexs <ale.osantos@hotmail.com>
2024-01-27 14:35:32 -0300

    adjust styles footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,16 +1,26 @@
+import { FaLinkedin } from "react-icons/fa";
 import Icon from "../../assets/logo.svg";
-import { Container, Logo } from "./styles";
+import { Container, Content, Logo } from "./styles";
 
 export function Footer() {
   return (
     <Container>
-      <Logo>
-        <img src={Icon} alt="Logo" />
+      <Content>
+        <Logo>
+          <img src={Icon} alt="Logo" />
+          <h1>food explorer</h1>
+        </Logo>
 
-        <h1>food explorer</h1>
-      </Logo>
-
-      <h2>© 2023 - Todos os direitos reservados.</h2>
+        <div>
+          <h2>© 2023 - Todos os direitos reservados.</h2>
+          <a
+            target="_black"
+            href="https://www.linkedin.com/in/alexs-oliveiraSantos/"
+          >
+            <FaLinkedin />
+          </a>
+        </div>
+      </Content>
     </Container>
   );
 }
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -2,36 +2,63 @@ import { styled } from "styled-components";
 
 export const Container = styled.footer`
   width: 100%;
+  background-color: ${({ theme }) => theme.COLORS.DARK_600};
+`;
+
+export const Content = styled.div`
+  width: 100%;
   height: 7.7rem;
+  padding: 4rem 1.8rem;
 
   display: flex;
   align-items: center;
   justify-content: space-between;
+  gap: 0.5rem;
+  margin: 0 auto;
 
-  padding: 4rem 1.8rem;
+  > div:last-child {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    text-align: center;
+    gap: 0.5rem;
 
-  background-color: ${({ theme }) => theme.COLORS.DARK_600};
+    > h2 {
+      font-size: clamp(1.1rem, 2.6vw, 1.4rem);
+      color: ${({ theme }) => theme.COLORS.LIGHT_200};
+      font-weight: 400;
+    }
 
-  > h2 {
-    font-size: clamp(0.7rem, 0.7rem + 0.7vw, 2.5rem);
-    color: ${({ theme }) => theme.COLORS.LIGHT_200};
+    > a {
+      display: flex;
+      align-items: center;
+      padding-bottom: 0.3rem;
+
+      > svg {
+        color: ${({ theme }) => theme.COLORS.LIGHT_200};
+        font-size: 1.6rem;
+      }
+    }
   }
 `;
 
 export const Logo = styled.div`
   display: flex;
   align-items: center;
+  gap: clamp(0.5rem, 0.5vw, 1rem);
 
   > img {
-    height: clamp(1.2rem, 1.2rem + 0.943vw, 2.8rem);
+    width: clamp(2rem, 3vw, 3rem);
+    height: clamp(2rem, 3vw, 3rem);
 
     margin-right: 0.3rem;
     filter: grayscale(1);
   }
 
   > h1 {
-    font-size: clamp(1.2rem, 1rem + 0.943vw, 2.8rem);
-
+    font-size: clamp(1.6rem, 3vw, 2.8rem);
+    white-space: nowrap;
     font-weight: 700;
     color: ${({ theme }) => theme.COLORS.LIGHT_700};
   }
